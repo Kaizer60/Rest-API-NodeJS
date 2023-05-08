3b3a57632f71c10209d7622709218be2e0c65061
Kaizer60 <rutchanon.sbo@gmail.com>
2023-05-08 16:01:38 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,151 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express();
+
+//แปลง json obj เป็น javascript obj 
+app.use(express.json())
+
+//MySQL Connection
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mysql_nodejs'
+})
+
+con.connect((err) => {
+    if(err){
+        console.log('Error connecting to MySQL database = ', err)
+        return
+    }
+    else{
+        console.log('MySQL successfully connected!')
+    }
+})
+
+//Create
+app.post("/create", async (req, res) => {
+    const{email,name, password} = req.body
+
+    try{
+        con.query(
+            "INSERT INTO users(email, fullname, password) VALUES(?,?,?)",
+            [email, name, password],
+            (err, results, fields) => {
+                if(err){
+                    console.log("Error while inserting a user into the database", err)
+                    return res.status(400).send();
+                }
+                else{
+                    return res.status(201).json({mesasage: "New user successfully created!"})
+                }
+            }
+        )
+    }catch(err){
+        console.log(err)
+        return res.status(500),send();
+    }
+})
+
+//Read
+app.get("/read", async (req, res) => {
+    try{
+        con.query(
+            "SELECT * FROM users",
+            (err, results, fields) => {
+                if(err){
+                    console.log("Error while inserting a user into the database", err)
+                    return res.status(400).send();
+                }
+                else{
+                    return res.status(200).json(results)
+                }
+            }
+        )
+    }catch(err){
+        console.log(err)
+        return res.status(500),send();
+    }
+})
+
+//Read single users
+app.get("/read/single/:email", async (req, res) => {
+    const email = req.params.email
+
+    try{
+        con.query(
+            "SELECT * FROM users WHERE email = ?",
+            [email],
+            (err, results, fields) => {
+                if(err){
+                    console.log("Error while inserting a user into the database", err)
+                    return res.status(400).send();
+                }
+                else{
+                    return res.status(200).json(results)
+                }
+            }
+        )
+    }catch(err){
+        console.log(err)
+        return res.status(500),send();
+    }
+})
+
+//Update
+app.patch("/update/:id", async (req, res) => {
+    const id = req.params.id
+    const newPassword = req.body.newPassword
+
+    try{
+        con.query(
+            "UPDATE users SET password = ? WHERE id = ?",
+            [newPassword, id],
+            (err, results, fields) => {
+                if(err){
+                    console.log("Error while inserting a user into the database", err)
+                    return res.status(400).send();
+                }
+                else{
+                    return res.status(200).json({mesasage: "User password updated successfully!"})
+                }
+            }
+        )
+    }catch(err){
+        console.log(err)
+        return res.status(500),send();
+    }
+})
+
+//Delete
+app.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id
+
+    try{
+        con.query(
+            "DELETE FROM users WHERE id = ?",
+            [id],
+            (err, results, fields) => {
+                if(err){
+                    console.log("Error while inserting a user into the database", err)
+                    return res.status(400).send();
+                }
+                //เช็คว่ามีการลบข้อมูลจริงหรือไม่
+                //กรณีที่ไม่มีข้ออมูลที่ต้องการลบ
+                if(results.affectedRows === 0){
+                    return res.status(404).json({mesasage: "No user with that id!"})
+                }
+                //กรณีทำการลบสำเร็จ
+                else{
+                    return res.status(200).json({mesasage: "User deleted successfully!"})
+                }
+            }
+        )
+    }catch(err){
+        console.log(err)
+        return res.status(500),send();
+    }
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
---
+++ users.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2023 at 10:53 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mysql_nodejs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `fullname`, `password`) VALUES
+(1, 'rutchanon.sbo@gmail.com', 'rutchanon somboon', 'Benz1234'),
+(2, 'Benzzy.sbo@gmail.com', 'benzzy somboon', 'Benzzy60');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
